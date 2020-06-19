9605058d9269e92b62bcb2c9281dc4e6549fd6f2
Pk11 <epicpkmn11@outlook.com>
2020-06-19 03:24:07 -0500

    Update to bootstrap 5 for better card grids

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,54 +1,12 @@
+let item
 function search(query) {
+	console.log(query);
 	Array.from(document.getElementById("card-container").children).forEach(function(r) {
-		if(r.id.toLowerCase().includes(query.toLowerCase()))
-			r.classList.remove("d-none");
-		else
-			r.classList.add("d-none");
-	});
-}
-
-if(!localStorage.sortDirection)
-	localStorage.sortDirection = false;
-if(!localStorage.sortProp)
-	localStorage.sortProp = "title";
-
-document.getElementById("sort-" + localStorage.sortProp).classList.add("btn-secondary");
-document.getElementById("sort-" + localStorage.sortProp).classList.remove("btn-outline-secondary");
-document.getElementById("sort-direction").innerText = parseInt(localStorage.sortDirection) ? "Descending" : "Ascending";
-sort();
-
-function toggleSortDirection() {
-	localStorage.sortDirection ^= 1;
-	document.getElementById("sort-direction").innerText = parseInt(localStorage.sortDirection) ? "Descending" : "Ascending";
-	sort();
-}
-
-function sort(prop) {
-	if(prop) {
-		document.getElementById("sort-" + prop).classList.remove("btn-outline-secondary");
-		document.getElementById("sort-" + prop).classList.add("btn-secondary");
-		document.getElementById("sort-" + localStorage.sortProp).classList.add("btn-outline-secondary");
-		document.getElementById("sort-" + localStorage.sortProp).classList.remove("btn-secondary");
-		localStorage.sortProp = prop;
-	}
-
-	let sorted = Array.from(document.getElementsByClassName("card")).sort(function(l, r) {
-		return ((l.dataset[localStorage.sortProp].toLowerCase() < r.dataset[localStorage.sortProp].toLowerCase()) ^ localStorage.sortDirection) ? -1 : 1;
-	});
-	let container = document.getElementById("card-container");
-	container.innerHTML = "";
-	let row = document.createElement("div");
-	row.classList = "row mb-3";
-	for(let i in sorted) {
-		let col = document.createElement("div");
-		col.classList.add("col-md-3");
-		col.appendChild(sorted[i]);
-		row.appendChild(col);
-		if(i % 4 == 3) {
-			container.appendChild(row);
-			row = document.createElement("div");
-			row.classList = "row mb-3";
+		let card = r.children[0];
+		for(let item in card.dataset) {
+			if(card.dataset[item].toLowerCase().includes(query.toLowerCase()))
+				return r.classList.remove("d-none");
 		}
-	}
-	container.appendChild(row);
+		r.classList.add("d-none");
+	});
 }
---
+++ assets/js/sort.js
@@ -0,0 +1,37 @@
+if(!localStorage.sortDirection)
+	localStorage.sortDirection = false;
+if(!localStorage.sortProp)
+	localStorage.sortProp = "title";
+
+document.getElementById("sort-" + localStorage.sortProp).classList.add("btn-secondary");
+document.getElementById("sort-" + localStorage.sortProp).classList.remove("btn-outline-secondary");
+document.getElementById("sort-direction").innerText = parseInt(localStorage.sortDirection) ? "Descending" : "Ascending";
+sort();
+
+function toggleSortDirection() {
+	localStorage.sortDirection ^= 1;
+	document.getElementById("sort-direction").innerText = parseInt(localStorage.sortDirection) ? "Descending" : "Ascending";
+	sort();
+}
+
+function sort(prop) {
+	if(prop) {
+		document.getElementById("sort-" + prop).classList.remove("btn-outline-secondary");
+		document.getElementById("sort-" + prop).classList.add("btn-secondary");
+		document.getElementById("sort-" + localStorage.sortProp).classList.add("btn-outline-secondary");
+		document.getElementById("sort-" + localStorage.sortProp).classList.remove("btn-secondary");
+		localStorage.sortProp = prop;
+	}
+
+	let sorted = Array.from(document.getElementsByClassName("card")).sort(function(l, r) {
+		return ((l.dataset[localStorage.sortProp].toLowerCase() < r.dataset[localStorage.sortProp].toLowerCase()) ^ localStorage.sortDirection) ? -1 : 1;
+	});
+	let container = document.getElementById("card-container");
+	container.innerHTML = "";
+	sorted.forEach(function(r) {
+		let col = document.createElement("div");
+		col.classList = "col mb-3";
+		col.appendChild(r);
+		container.appendChild(col);
+	});
+}
