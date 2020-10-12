e4fabf72bc58c0511f4383a13af6007b474b4de2
Pk11 <epicpkmn11@outlook.com>
2020-10-12 00:47:21 -0500

    Make QR popup less janky

--- assets/js/popupQR.js
+++ assets/js/popupQR.js
@@ -1,9 +1,18 @@
 function toggleQR(path, name) {
-	if(path)
-		document.getElementById("popup-image").src = path;
-	if(name)
-		document.getElementById("popup-label").textContent = name;
-	let container = document.getElementById("popup-container");
-	container.style.display = "";
-	container.classList.toggle("d-none");
+	let image = document.getElementById("popup-image");
+	if(!image) {
+		if(path) {
+			let img = document.createElement("img");
+			img.src = path;
+			img.classList = "mx-auto d-block";
+			img.id = "popup-image";
+			img.alt = name ? ("QR code for " + name) : "QR code";
+			document.getElementById("popup").appendChild(img);
+			document.getElementById("popup-container").classList.remove("d-none");
+		}
+		document.getElementById("popup-label").textContent = name ?? "";
+	} else {
+		image.remove();
+		document.getElementById("popup-container").classList.add("d-none");
+	}
 }
