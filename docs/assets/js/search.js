a77c1d0ce8a15e3c5a92142f82f5bd47ab87d8d0
Pk11 <epicpkmn11@outlook.com>
2021-06-14 07:40:53 -0500

    Fuzzy search for title
    Specifically, match the query's letters in order, but with anything allowed between them. So you can search "twlmenu" or "twilight++" for "TWiLight Menu++", but not "menu twilight"

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,8 +1,9 @@
 function search(query) {
+	let regex = new RegExp(query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\\?./g, "$&.*"), "gi");
 	Array.from(document.getElementById("card-container").children).forEach(function(r) {
 		let card = r.children[0];
 		for(let item in card.dataset) {
-			if(card.dataset[item].toLowerCase().includes(query.toLowerCase()))
+			if(item == "title" ? card.dataset[item].match(regex) : card.dataset[item].toLowerCase().includes(query.toLowerCase()))
 				return r.classList.remove("d-none");
 		}
 		r.classList.add("d-none");
