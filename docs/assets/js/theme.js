3b30dfea856be6bce52d76a460a88aeafbdd029e
Pk11 <epicpkmn11@outlook.com>
2022-03-26 20:34:42 -0500

    Make Universal-DB 83% more beautiful with this one simple trick

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,9 +1,14 @@
 function loadTheme() {
 	const themeCSS = document.getElementById("theme-css");
-	if(!localStorage.theme || localStorage.theme == "default")
-		themeCSS.href = "/assets/css/style.css";
-	else
-		themeCSS.href = "/assets/css/" + localStorage.theme + ".css";
+	const today = new Date();
+	if(today.getMonth() == 3 && today.getDate() == 1) {
+		themeCSS.href = "/assets/css/83-percent.css";
+	} else {
+		if(!localStorage.theme || localStorage.theme == "default")
+			themeCSS.href = "/assets/css/style.css";
+		else
+			themeCSS.href = "/assets/css/" + localStorage.theme + ".css";
+	}
 }
 
 function setTheme(theme) {
