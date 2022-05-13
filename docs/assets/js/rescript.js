82fef36614cbab40f46d57e63e91e0bd0dcee288
Pk11 <epicpkmn11@outlook.com>
2022-05-12 19:58:33 -0500

    Try fix rescript running on browsers it shouldn't

--- docs/assets/js/rescript.js
+++ docs/assets/js/rescript.js
@@ -1,3 +1,5 @@
+{...{}}; // Syntax error in anything before ECMAScript 9, we don't want to run this code on old browsers
+
 // Make navbar not open on hover
 Array.from(document.getElementsByClassName("hover-content")).forEach(r => {
 	r.classList.remove("hover-content");
