66046bc18cb8f9b036d92eaf8350e066219fae0b
Pk11 <epicpkmn11@outlook.com>
2022-05-19 15:49:38 -0500

    Revert "Try fix rescript running on browsers it shouldn't"
    This reverts commit 82fef36614cbab40f46d57e63e91e0bd0dcee288.

--- docs/assets/js/rescript.js
+++ docs/assets/js/rescript.js
@@ -1,5 +1,3 @@
-{...{}}; // Syntax error in anything before ECMAScript 9, we don't want to run this code on old browsers
-
 // Make navbar not open on hover
 Array.from(document.getElementsByClassName("hover-content")).forEach(r => {
 	r.classList.remove("hover-content");
