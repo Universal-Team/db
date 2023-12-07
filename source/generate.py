aae87d4ae42e6b7cfd835dcf201e73494cba63c0
Pk11 <pk11@outlook.jp>
2023-12-07 17:34:17 -0600

    Yeet dumb images

--- source/generate.py
+++ source/generate.py
@@ -363,6 +363,7 @@ def main(sourceFile, docsDir: str, ghToken: str, priorityOnlyMode: bool) -> None
 					if "update_notes" not in app and release["body"] != "" and release["body"] is not None:
 						app["update_notes_md"] = release["body"].replace("\r\n", "\n")
 						app["update_notes"] = requests.post("https://api.github.com/markdown", headers=header if header else None, json={"text": release["body"], "mode": "gfm" if "github" in app else "markdown", "context": app["github"] if "github" in app else None}).text
+						app["update_notes"] = re.sub(r'\n<p dir="auto"><a target="_blank" rel="noopener noreferrer" href="https:\/\/private-user-images.githubusercontent\.com.*?<\/p>\n', "", app["update_notes"])
 
 					if "updated" not in app:
 						app["updated"] = release["published_at"]
